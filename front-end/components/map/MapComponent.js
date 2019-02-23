d3a867eddb95e01c1b6fc77daaecda12473cadbb
Simon Davis <simon_freelance@hotmail.com>
2019-02-23 18:18:27 -0500

    Made map take up full screen

--- front-end/components/map/MapComponent.js
+++ front-end/components/map/MapComponent.js
@@ -11,16 +11,24 @@ const MapComponent = compose(
     googleMapURL:
       "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
     loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
+    containerElement: (
+      <div
+        style={{
+          position: "absolute",
+          top: "0",
+          bottom: "0",
+          left: "0",
+          right: "0"
+        }}
+      />
+    ),
     mapElement: <div style={{ height: `100%` }} />
   }),
   withScriptjs,
   withGoogleMap
 )(props => (
   <GoogleMap defaultZoom={8} defaultCenter={{ lat: -34.397, lng: 150.644 }}>
-    {props.isMarkerShown && (
-      <Marker position={{ lat: -34.397, lng: 150.644 }} />
-    )}
+    <Marker draggable={true} position={{ lat: -34.397, lng: 150.644 }} />
   </GoogleMap>
 ));
 
