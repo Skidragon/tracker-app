4df580e088cd8adcabf77d94a017d8d901150387
Simon Davis <simonfreelance1@gmail.com>
2019-03-28 14:40:18 -0400

    Added packages for config and configured webpack

--- front-end/next.config.js
+++ front-end/next.config.js
@@ -1,4 +1,21 @@
 // next.config.js
+const { parsed: localEnv } = require("dotenv").config();
+const webpack = require("webpack");
 const withCSS = require("@zeit/next-css");
 const withTypescript = require("@zeit/next-typescript");
-module.exports = withCSS(withTypescript());
+const tsConfig = {};
+const cssConfig = {};
+
+// https://github.com/JerryCauser/next-compose
+const compose = require("next-compose");
+
+module.exports = compose([
+  [withCSS, cssConfig],
+  [withTypescript, tsConfig],
+  {
+    webpack: config => {
+      config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
+      return config;
+    }
+  }
+]);
