0f4ffec3fd06bf904376c884848eca458f03019e
Simon Davis <simonfreelance1@gmail.com>
2019-03-28 22:03:23 -0400

    Build fix 1

--- front-end/components/map/state-and-methods/index.ts
+++ front-end/components/map/state-and-methods/index.ts
@@ -1,4 +1,4 @@
-export {default as useInfoWindow} from "./useInfoWindow";
+export {default as useInfoWindow} from "./UseInfoWindow";
 export {default as useMarker} from "./useMarker";
 export {default as usePolyline} from "./usePolyline";
 export {default as useTrash} from "./useTrash";
