8705916d6533d2060c64837d06cf4497d47e807b
Simon Davis <simonfreelance1@gmail.com>
2019-03-31 23:16:06 -0400

    Created StepsStatusBar component for save trip process

---
+++ front-end/components/map/SaveTripProcess/StepsStatusBar.tsx
@@ -0,0 +1,81 @@
+import {Steps, Card, Button} from "antd";
+import styled from "styled-components";
+
+const Step = Steps.Step;
+
+interface Props {
+  step: number;
+  setStep: Function;
+}
+const StepsWrapper = styled.div`
+  position: absolute;
+  left: 3%;
+  top: 22%;
+`;
+const StepButtonGroup = styled.div`
+  display: flex;
+  justify-content: space-between;
+`;
+const ExitBtn = styled(Button)`
+  display: ${(props: {step: number}) =>
+    props.step === 0 ? "flex" : "none"};
+`;
+const PreviousBtn = styled(Button)`
+  display: ${(props: {step: number}) =>
+    props.step > 0 ? "flex" : "none"};
+`;
+const NextBtn = styled(Button)`
+  display: flex;
+`;
+
+const StepsStatusBar: React.SFC<Props> = ({step, setStep}) => {
+  if (step === -1) {
+    return null;
+  } else {
+    return (
+      <StepsWrapper>
+        <Card>
+          <Steps direction="vertical" current={step}>
+            <Step
+              title="Go to location"
+              description="The location needs to be visible before taking snapshot."
+              onClick={() => setStep(0)}
+            />
+            <Step
+              title="Take Snapshot"
+              description="Creates a desired location image for trip."
+              onClick={() => setStep(1)}
+            />
+            <Step
+              title="Finish Trip"
+              description="Fill out the rest of the details then save."
+              onClick={() => setStep(2)}
+            />
+          </Steps>
+          <StepButtonGroup>
+            <ExitBtn step={step} type="danger" onClick={() => setStep(-1)}>
+              Exit
+            </ExitBtn>
+            <PreviousBtn
+              step={step}
+              onClick={() => setStep((prevState: number) => prevState - 1)}
+              disabled={step === 0 ? true : false}
+            >
+              Previous
+            </PreviousBtn>
+            <NextBtn
+              step={step}
+              type="primary"
+              onClick={() => setStep((prevState: number) => prevState + 1)}
+              disabled={step > 2 ? true : false}
+            >
+              {step >= 2 ? "Done" : "Next"}
+            </NextBtn>
+          </StepButtonGroup>
+        </Card>
+      </StepsWrapper>
+    );
+  }
+};
+
+export default StepsStatusBar;
